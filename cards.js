187a8bc36772cec257e194e30032a6c62807efc6
efhiii <efhiii2000@gmail.com>
2024-03-04 18:47:47 -0800

    Ice cream and egg text

--- cards.js
+++ cards.js
@@ -366,11 +366,11 @@ const jokerTexts = [
   ],
   [],
   [
-    ['Egg', 'Gains ${moneyc}$3${endc} of<br>${numc}sell value${endc} at<br>end of round<br>${shadowc}(Currently ${moneyc}+$${jokerValue * 3}${endc})${endc}'],
+    ['Egg', 'Gains ${moneyc}$3${endc} of<br>${numc}sell value${endc} at<br>end of round'],
     ['Burglar', 'When ${numc}Blind${endc} is selected,<br>gain ${chipc}+3${endc} Hands and<br>${numc}lose all discards${endc}'],
     ['Blackboard', '${prodc}3${endc} Mult if all<br>cards held in hand<br>are ${spadec} or ${clubc}'],
     ['Runner', 'Gains ${chipc}+10${endc} Chips<br>if played hand<br>contains a ${numc}Straight${endc}<br>${shadowc}(Currently ${chipc}+${20 + jokerValue * 10}${endc} Chips)${endc}'],
-    ['Ice Cream', '${chipc}+100${endc} Chips<br>${chipc}-5${endc} Chips for<br>every hand played<br>${shadowc}(Currently ${chipc}${jokerValue > 20 ? "" : "+"}${Math.max(0, 100 - jokerValue * 5)}${endc} Chips)${endc}'],
+    ['Ice Cream', '$${chipc}${jokerValue > 20 ? "" : "+"}${Math.max(0, 100 - jokerValue * 5)}${endc} Chips<br>${chipc}-5${endc} Chips for<br>every hand played'],
     ['DNA', 'If ${numc}first hand${endc} of round<br>has only ${numc}1${endc} card, add a<br>permanent copy to deck<br>and draw it to ${numc}hand${endc}'],
     ['Splash', 'Every ${numc} played card${endc}<br>counts in scoring'],
     ['Blue Joker', '${chipc}+2${endc} Chips for each<br>remaining card in ${numc}Deck${endc}<br>${shadowc}(Currently ${chipc}+${104 + jokerValue * 2}${endc} Chips)${endc}'],
